c9fdd916f29cf5a0e890491ce5df73af6b8c2efa
syarin-x <magicspell.max.mix.mutax@gmail.com>
2022-12-10 19:26:10 +0900

    Support change reval.

--- src/main.zig
+++ src/main.zig
@@ -5,23 +5,16 @@ const dprint = std.debug.print;
 
 pub fn main() !void {
     const stderr = std.io.getStdErr();
-    const ostream = std.io.getStdOut();
+    const stdout = std.io.getStdOut().writer();
 
     const args = std.os.argv;
     if (args.len != 2) {
         _ = try stderr.write("[error] invalid argument num.\n");
     }
 
-    _ = try ostream.write(".intel_syntax noprefix\n");
-    _ = try ostream.write(".global main\n");
-    _ = try ostream.write("main:\n");
-    _ = try ostream.write("  mov rax, 12\n");
-    _ = try ostream.write("  ret\n");
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    _ = try stdout.writeAll(".intel_syntax noprefix\n");
+    _ = try stdout.writeAll(".global main\n");
+    _ = try stdout.writeAll("main:\n");
+    _ = try stdout.print("  mov rax, {s}\n", .{args[1]});
+    _ = try stdout.writeAll("  ret\n");
 }
